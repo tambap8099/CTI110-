7d085647ce35ae09579097d2fb6287399c64b8fd
tambap8099 <tambap8099@student.faytechcc.edu>
2024-06-20 13:45:41 -0400

    Add files via upload

---
+++ Peter_Tamba.py
@@ -0,0 +1,7 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: D:/school work/CTI-110-1001/TambaPeter.py
+Enter your first name: Peter
+Enter your last name: Tamba
+hello, Peter Tamba! Welcome to CTI-110
